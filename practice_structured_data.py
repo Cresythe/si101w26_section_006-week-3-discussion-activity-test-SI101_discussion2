494d5b38ab0587915092514f0d11dda1825600c8
Corey <106111373+Cresythe@users.noreply.github.com>
2026-01-20 20:47:02 +0000

    Challenges done

--- practice_structured_data.py
+++ practice_structured_data.py
@@ -33,9 +33,14 @@ movies = [
 # - last_rating
 
 # Write your code below
-
-
-
+first_title = movies[0]['title'] 
+second_year = movies[1]['year']
+first_ratings = movies[0]['ratings']
+last_rating = movies[1]['ratings'][2]
+print(f"First title is {first_title}")
+print(f"second year is {second_year}")
+print(f"first ratings are {first_ratings}")
+print(f"last ratings is {last_rating}")
 
 # --------------------------------------------------
 # Part 3: Updating Nested Data
@@ -47,8 +52,14 @@ movies = [
 
 # Write your code below
 
+movies[0]['year'] = 2011 
+movies[1]['ratings'].append(10) 
+movies[1]['title'] = "The Matrix Reloaded"
+movies[1]['genre'] = "Action/Sci-fi"
 
-
+print(f"The First year is {movies[0]['year']}")
+print(f"The second ratings are {movies[1]['ratings']}") 
+print(f"The second title is {movies[1]['title']}") 
 
 # --------------------------------------------------
 # Part 4: Working with Strings
@@ -59,8 +70,9 @@ movies = [
 # - converts the title to title case
 
 # Write your code below
+clean_title = movies[0]['title'].strip().title() 
 
-
+print(f"The clean title is {clean_title}, The original title is {movies[0]['title']}")
 
 
 # --------------------------------------------------
@@ -71,7 +83,8 @@ movies = [
 # - the last character of clean_title
 
 # Write your code below
-
+bookends  = clean_title[0]+ clean_title[-1]
+print(bookends)
 
 
 
@@ -85,9 +98,10 @@ movies = [
 # Do not hard-code the final string.
 
 # Write your code below
-
+summary = f"Movie: {clean_title} ({movies[0]['year']})"
 
 
 
 # Optional: print summary to check your work
-# print(summary)
+print(summary)
+print(movies)
